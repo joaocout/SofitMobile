def3b78322d64a988cf253e13963efd6da951809
joaocout <jpedro0801@gmail.com>
2022-05-07 00:34:03 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  rules: {
+    'prefer-const': 'warn',
+    'spaced-comment': 'warn',
+    'no-console': 'warn',
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
+        'no-undef': 'off',
+      },
+    },
+  ],
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+## Sofit Mobile Challenge
